a15c1a7beef2da5c86b3d33e186f272ac83f87f7
GAUTAM MANAK <106014185+gautammanak1@users.noreply.github.com>
2024-08-18 03:17:25 +0530

    Update NavBar.jsx

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, Menu } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link, useNavigate } from 'react-router-dom';
 import { signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth';
